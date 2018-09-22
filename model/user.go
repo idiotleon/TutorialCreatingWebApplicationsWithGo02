415b000ec96226dec3aea9abb1cfa27427727ebd
Leon Lu <yanglu@microsoft.com>
2018-09-21 22:28:45 -0700

    demo: Updating a PostgresQL database

--- model/user.go
+++ model/user.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"encoding/base64"
 	"fmt"
+	"log"
 	"time"
 )
 
@@ -31,13 +32,20 @@ func Login(email, password string) (*User, error) {
 		FROM public.User
 		WHERE email = $1
 		AND password = $2`, email, pwd)
-	fmt.Println("pwd: ", pwd)
 	err := row.Scan(&result.ID, &result.Email, &result.FirstName, &result.LastName)
 	switch {
 	case err == sql.ErrNoRows:
 		return nil, fmt.Errorf("User not found")
 	case err != nil:
 		return nil, err
+	}
+	t := time.Now()
+	_, err = db.Exec(`
+		UPDATE public.user
+		SET lastlogin = $1
+		WHERE id = $2`, t, result.ID)
+	if err != nil {
+		log.Printf("Failed to update login time for user %v to %v: %v", result.Email, t, err)
 	}
 	return result, nil
 }
